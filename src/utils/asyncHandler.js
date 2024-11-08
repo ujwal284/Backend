c623faca2e2f954a5b87dab0729bc5d914f82780
ujwal284 <ujwalkhanal284@gmail.com>
2024-11-08 10:59:24 +0545

    added user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,3 +5,17 @@ const asyncHandler = (requestHandler) => {
 }
 
 export { asyncHandler }
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+
+//     }
+// }
