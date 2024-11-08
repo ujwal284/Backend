5214a1d9a9f3d3556b69e201a42a389372806c43
ujwal284 <ujwalkhanal284@gmail.com>
2024-11-08 17:35:30 +0545

    all setup configured

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-
+    credentials: true
 }))
 
 app.use(express.json({ limit: "16kb" }))
@@ -16,6 +16,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// routes import 
+import userRouter from './routes/user.routes.js';
 
+
+
+//routes declaretion
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 export { app }
