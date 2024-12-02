6aa19107a6f51eb378f25ba2d9b91bccc822825f
ujwal284 <ujwalkhanal284@gmail.com>
2024-12-02 09:23:41 +0545

    fixed bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -71,7 +71,7 @@ userSchema.methods.generateAccessToken = function () {
     },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: process.env, ACCESS_TOKEN_EXPIRT
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
@@ -82,7 +82,7 @@ userSchema.methods.generateRefreshToken = function () {
     },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env, REFRESH_TOKEN_EXPIRT
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -4,7 +4,7 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
 
-    videoFiile: {
+    videoFile: {
         type: String, // clounary url
         required: true
     },
